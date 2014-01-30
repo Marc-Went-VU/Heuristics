c5e7591996bb7d5fb2aa52dd03fad263af48ebe2
Dunky13 <mwent@live.nl>
2014-01-30 23:32:14 +0100

    Apparently this is the final commit

---
+++ Heuristics/src/tiling/own/Int.java
@@ -0,0 +1,21 @@
+package tiling.own;
+
+public class Int
+{
+	private int i;
+
+	public Int(int i)
+	{
+		this.i = i;
+	}
+
+	public int getI()
+	{
+		return this.i;
+	}
+
+	public void add(int j)
+	{
+		i += j;
+	}
+}
